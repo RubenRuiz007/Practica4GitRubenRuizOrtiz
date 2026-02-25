5f888d1eed8403ac7e2c3f40a34f67c9c8d3f442
rruiort <rruizortiz951@email.com>
2026-02-25 15:15:43 +0100

    Commit para perder y recuperar

--- src/com/ejemplo/p4/Main.java
+++ src/com/ejemplo/p4/Main.java
@@ -4,5 +4,6 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("Practica 4 - Git casos reales");
     System.out.println("Nombre Apellidos");
+    System.out.println("Mensaje añadido en el punto 9 - REFLOG");
   } 
 }
