54d8058baf617147b262d03f822b8db26c8d9f65
rruiort <rruizortiz951@email.com>
2026-02-25 12:31:25 +0100

    Inicio proyecto Practica 4

---
+++ src/com/ejemplo/p4/Main.java
@@ -0,0 +1,8 @@
+package com.ejemplo.p4;
+
+public class Main { 
+  public static void main(String[] args) {
+    System.out.println("Practica 4 - Git casos reales");
+    System.out.println("Nombre Apellidos");
+  } 
+}
