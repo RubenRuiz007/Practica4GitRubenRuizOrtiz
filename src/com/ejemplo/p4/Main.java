59f6dddef48599f7184fec371b30477d16c73966
rruiort <rruizortiz951@email.com>
2026-02-26 11:45:59 +0100

    Actualizar mensajes de salida en Main

--- src/com/ejemplo/p4/Main.java
+++ src/com/ejemplo/p4/Main.java
@@ -4,6 +4,7 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("Practica 4 - Git casos reales");
     System.out.println("Nombre Apellidos");
-    System.out.println("Mensaje añadido en el punto 9 - REFLOG");
+    System.out.println("Mensaje añadido en el punto 3.1");
+    System.out.println("Nueva línea en el punto 4.1");
   } 
 }
